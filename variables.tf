860cd1dea77dc99f742ed132434b6503ffc093ef
hariharan <hariharan.natarajan@nordcloud.com>
2022-04-01 17:33:59 +0200

    Commit AWS and Azure Code changes

--- variables.tf
+++ variables.tf
@@ -39,9 +39,15 @@ variable "public_subnet_cidrs" {
   description = "List of public cidrs, for every availability zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
 }
 
+variable "public_subnet_cidrs_eu-central-1b" {
+  type        = list(any)
+  default   = ["10.0.3.0/24"]
+  description = "List of public cidrs, for every availability zone you want you need one. Example: 10.0.0.0/24 and 10.0.1.0/24"
+}
+
 variable "availability_zones" {
   type        = list(any)
-  default   = ["eu-central-1a"]
+  default   = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   description = "List of availability zones you want. Example: eu-central-1a and eu-central-1b"
 }
 
