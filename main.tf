f3313da434899130447a12508a94850be1e04b07
hariharan <hariharan.natarajan@nordcloud.com>
2022-04-01 18:33:24 +0200

    Commit AWS and Azure Code changes

--- main.tf
+++ main.tf
@@ -180,7 +180,7 @@ resource "aws_elb" "bar" {
   idle_timeout                = 400
   connection_draining         = true
   connection_draining_timeout = 400
-  #subnets                     = var.public_subnet_cidrs_eu-central-1a
+  subnets                     = module.public_subnet.ids
 
   tags = {
     Name = "Website-terraform-elb"
